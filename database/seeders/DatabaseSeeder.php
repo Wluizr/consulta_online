1c892546b412e5d2a91b40c099196ab44d306f85
wluizr <tom.luizr@gmail.com>
2025-01-30 17:32:59 -0300

    chore(seeder): Atualizando a ordem de execução dos seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersSeeder::class,
+            CidadesSeeder::class,
+            MedicosSeeder::class,
+            PacientesSeeder::class,
+            ConsultasSeeder::class,
         ]);
+
     }
 }
